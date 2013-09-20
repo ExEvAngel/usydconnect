bea5fffbd20c08523e9cd47bdd2fb980f942b6a9
Henry <lvl2pillow@gmail.com>
2013-09-21 07:56:57 +1000

    Added commenting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	# Returns "No Title" if no provided title, else use provided title.
+	def full_title(page_title)
+		base_title = "No Title"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title}"
+		end
+	end
 end
