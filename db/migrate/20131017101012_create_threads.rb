6d87c6404ac9f2fb3543d7ff9c12156678f38276
catherinehelen <cyoushey@gmail.com>
2013-10-18 12:49:53 +1100

    Update 20131017101012_create_threads.rb

--- db/migrate/20131017101012_create_threads.rb
+++ db/migrate/20131017101012_create_threads.rb
@@ -1,8 +1,8 @@
 class CreateThreads < ActiveRecord::Migration
   def change
     create_table :threads do |t|
-	  t.integer :user_id, :null => false
-	  t.string :title, :limit => 200, :null => false
+      t.integer :user_id, :null => false
+      t.string :title, :limit => 200, :null => false
       t.string :body, :limit => 500, :null => false
       t.boolean :is_closed, :default => false
       t.integer :no_flags, :default => 0
