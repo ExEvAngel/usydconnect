cfdc9c681871859148fcd91bc7b23b355ac04fb6
catherinehelen <cyoushey@gmail.com>
2013-10-18 13:43:06 +1100

    trying to make a thread belong to a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   validates :username, :uniqueness => true, :presence => true
   validates :password, :presence => true
  
+  def self.get_user_id(user)
+    @user = User.where(username: user)
+  	if !@user.empty?
+      1
+	end
+  end
+
 end
