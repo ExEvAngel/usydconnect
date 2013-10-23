8e7d1159dc4ee4dc7301f29ca9592973c16c5cea
Henry <lvl2pillow@gmail.com>
2013-10-24 00:19:00 +1100

    thread updates

--- db/schema.rb
+++ db/schema.rb
@@ -14,22 +14,22 @@
 ActiveRecord::Schema.define(version: 20131022224727) do
 
   create_table "achievements", force: true do |t|
-    t.string   "name",        limit: 30,  null: false
-    t.string   "description", limit: 500, null: false
-    t.integer  "condition",               null: false
-    t.string   "badge",       limit: 300
+    t.string   "name",       limit: 30,  null: false
+    t.string   "dscription", limit: 500, null: false
+    t.integer  "condition",              null: false
+    t.string   "badge",      limit: 300
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "athreads", force: true do |t|
     t.integer  "user_id"
-    t.string   "title",      limit: 200,                 null: false
-    t.string   "body",       limit: 500,                 null: false
+    t.string   "title",      limit: 200,                                 null: false
+    t.string   "body",       limit: 500,                                 null: false
     t.boolean  "is_closed",              default: false
     t.integer  "no_flags",               default: 0
     t.integer  "views",                  default: 0
-    t.datetime "Date"
+    t.datetime "Date",                   default: '2013-10-20 12:17:31'
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(version: 20131022224727) do
   create_table "comments", force: true do |t|
     t.integer  "user_id"
     t.integer  "athread_id"
-    t.string   "body",       limit: 500,             null: false
+    t.string   "body",       limit: 500,                                 null: false
     t.integer  "no_flags",               default: 0
     t.integer  "views",                  default: 0
-    t.datetime "date"
+    t.datetime "date",                   default: '2013-10-20 12:17:29'
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20131022224727) do
     t.integer  "user_id"
     t.string   "title",      limit: 200
     t.string   "body",       limit: 500
-    t.datetime "saved_at",               default: '2013-10-22 23:57:05'
+    t.datetime "saved_at",               default: '2013-10-20 12:17:29'
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 20131022224727) do
 
   create_table "moderators", force: true do |t|
     t.integer  "user_id"
-    t.date     "moderator_since", default: '2013-10-22'
+    t.date     "moderator_since", default: '2013-10-20'
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -111,6 +111,17 @@ ActiveRecord::Schema.define(version: 20131022224727) do
     t.datetime "updated_at"
   end
 
+  create_table "threads", force: true do |t|
+    t.string   "title",      limit: 200
+    t.string   "body",       limit: 500
+    t.boolean  "is_closed",              default: false
+    t.integer  "no_flags",               default: 0
+    t.integer  "views",                  default: 0
+    t.datetime "Date",                   default: '2013-10-17 10:20:55'
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "user_achievements", force: true do |t|
     t.integer  "user_id"
     t.integer  "achievement_id"
@@ -119,14 +130,13 @@ ActiveRecord::Schema.define(version: 20131022224727) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username",        limit: 16,                        null: false
-    t.string   "password",        limit: 16,                        null: false
-    t.string   "password_digest"
-    t.string   "email",           limit: 40,                        null: false
-    t.boolean  "is_staff",                   default: false
-    t.integer  "xp",                         default: 0
-    t.boolean  "is_banned",                  default: false
-    t.date     "member_since",               default: '2013-10-22'
+    t.string   "username",     limit: 16
+    t.string   "password",     limit: 16
+    t.string   "email",        limit: 40
+    t.boolean  "is_staff",                default: false
+    t.integer  "xp",                      default: 0
+    t.boolean  "is_banned",               default: false
+    t.date     "member_since"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
