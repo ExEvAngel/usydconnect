8c27727594e19e1bcab123f42ca07c7169f75590
Henry <lvl2pillow@gmail.com>
2013-10-24 15:38:51 +1100

    email deleted

--- app/mailers/email.rb
+++
@@ -1,7 +0,0 @@
-class Email < ActionMailer::Base
-  default from: "system@example.com"
-  
-  def signup_verification(@user, @code)
-    mail(to: @user.email, subject: "Usydconnect Verification")
-  end
-end
