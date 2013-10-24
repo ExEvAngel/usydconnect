daff14331f4b3abf690f4cff1bf1a1d85558b48f
catherinehelen <cyoushey@gmail.com>
2013-10-24 21:23:13 +1100

    AAHHH

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,19 @@ Tag.create([{name: 'General'}, {name: 'Assignment'}, {name: 'Content'}, {name: '
 Unitcode.create([{name: 'General'}, {name: 'COMP'}, {name: 'ELEC'}, {name: 'INFO'}, {name: 'ISYS'}])
 
 # Pinned Posts
- Athread.create(user_id: user.id, title: 'Rules' ,body: 'Before posting any threads/comments on the forum, all users must agree to abide by all the proceeding forum rules 
+ Athread.create(user_id: 1, title: 'Rules' ,body: 'Before posting any threads/comments on the forum, all users must agree to abide by all the proceeding forum rules 
  												- Do not include any threads and/or posts that contain any offensive language 
  												- Do not post any content that will reveal any personal information about any other member of the forum 
  												- Do not post any offensive or insulting content 
  												- Do not impersonate another forum member 
  												- Do not spam the forum with repeated threads', Date: Time.now)
 
- thread = Athread.create(user_id: user.id, title: 'Error' ,body: 'In the event of an error, please attempt the following fixes: 
+ thread = Athread.create(user_id: 1, title: 'Error' ,body: 'In the event of an error, please attempt the following fixes: 
  			Refresh your browser page, 
  			Restart your browser, 
  			Use a different browser to verify whether the problem persists', Date: Time.now)
 
- Comment.create(user_id: user.id, athread_id: thread.id, body: 'In the event that all these options fail, you may then notify one of the active moderators of the problem. 
+ Comment.create(user_id: 1, athread_id: thread.id, body: 'In the event that all these options fail, you may then notify one of the active moderators of the problem. 
  Please note that fixes may not be immediate and could require some time to find a solution if the problem is not localised to your machine.', date: Time.now)
 
 
